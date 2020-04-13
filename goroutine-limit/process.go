04d5273502cacaf700f94c3b0c5af36a7928ece0
Diego Bernardes <diego.bernardes@outlook.com>
2020-04-13 22:03:29 +0100

    Updated goroutine-limit from the episode

--- goroutine-limit/process.go
+++ goroutine-limit/process.go
@@ -6,16 +6,23 @@ import (
 	"time"
 )
 
-const concurrency = 1_000_000
+const concurrency = 10_000_000
 
 var counter int64
 
 func process() {
 	var wg sync.WaitGroup
 	wg.Add(concurrency)
+
+	chLimit := make(chan struct{}, 500_000)
+
 	for i := 0; i < concurrency; i++ {
+		chLimit <- struct{}{}
 		go func() {
-			defer wg.Done()
+			defer func() {
+				<-chLimit
+				wg.Done()
+			}()
 			atomic.AddInt64(&counter, 1)
 			time.Sleep(3 * time.Second)
 		}()
